fd011849894607a498a551f83335c04415121aed
Emmet BrickHacker <thomas.morta@post.cz>
2024-04-02 17:05:26 +0200

    Added control by thumb joystick

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType
     datalogger.createCV("send variable name", "drive"),
     datalogger.createCV("send variable value", code(50, -50))
     )
-    if (inicialised) {
+    if (inicialised && cross_control) {
         radio.sendValue("drive", code(-50, -50))
     }
 })
@@ -16,7 +16,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType
     datalogger.createCV("send variable name", "drive"),
     datalogger.createCV("send variable value", code(-50, 50))
     )
-    if (inicialised) {
+    if (inicialised && cross_control) {
         radio.sendValue("drive", code(-50, 50))
     }
 })
@@ -34,6 +34,7 @@ input.onButtonPressed(Button.AB, function () {
     datalogger.createCV("button", "A+B"),
     datalogger.createCV("inicialised", inicialised)
     )
+    cross_control = !(cross_control)
 })
 // Zakóduje informaci o požadovaném pohybu obou motorů do jediného čísla
 // - LM speed ... požadovaná rychlost levého motoru
@@ -41,13 +42,6 @@ input.onButtonPressed(Button.AB, function () {
 function code (LM_speed: number, RM_speed: number) {
     return (100 + LM_speed) * 1000 + (100 + RM_speed)
 }
-input.onButtonPressed(Button.B, function () {
-    datalogger.log(
-    datalogger.createCV("button", "B"),
-    datalogger.createCV("inicialised", inicialised)
-    )
-    log_thumb_joystick = !(log_thumb_joystick)
-})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     datalogger.log(
     datalogger.createCV("button", "D"),
@@ -55,7 +49,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType
     datalogger.createCV("send variable name", "drive"),
     datalogger.createCV("send variable value", code(50, 50))
     )
-    if (inicialised) {
+    if (inicialised && cross_control) {
         radio.sendValue("drive", code(50, 50))
     }
 })
@@ -66,11 +60,17 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType
     datalogger.createCV("send variable name", "drive"),
     datalogger.createCV("send variable value", code(50, -50))
     )
-    if (inicialised) {
+    if (inicialised && cross_control) {
         radio.sendValue("drive", code(50, -50))
     }
 })
-let log_thumb_joystick = false
+let steering_speed = 0
+let ratio = 0
+let steering = 0
+let speed = 0
+let X = 0
+let Y = 0
+let cross_control = false
 let inicialised = false
 radio.setGroup(128)
 inicialised = false
@@ -84,6 +84,52 @@ datalogger.setColumnTitles(
 )
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
 joystickbit.initJoystickBit()
-basic.showIcon(IconNames.House)
 inicialised = true
-log_thumb_joystick = false
+cross_control = false
+basic.forever(function () {
+    if (cross_control) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # . # #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . # . #
+            # . . . #
+            . # # # .
+            `)
+    }
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (!(cross_control)) {
+        Y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 0, 1023, -100, 100)
+        X = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 1023, 0, -100, 100)
+        speed = Math.sqrt(X ** 2 + Y ** 2)
+        if (speed > 100) {
+            speed = 100
+        } else if (speed < 15) {
+            speed = 0
+        }
+        if (Y < 0) {
+            speed = -1 * speed
+        }
+        steering = Math.abs(Y) - Math.abs(X)
+        ratio = speed / 100
+        steering_speed = speed * ratio
+        if (steering_speed < 15 && steering_speed > -15) {
+            steering_speed = 0
+        }
+        if (X < 0) {
+            radio.sendValue("drive", code(speed, steering_speed))
+        } else {
+            radio.sendValue("drive", code(steering_speed, speed))
+        }
+        basic.pause(100)
+    }
+})
